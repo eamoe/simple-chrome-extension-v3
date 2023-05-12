b3045e6d344901659f659890598af29083165f19
Eugene Menski <eugene.menski@gmail.com>
2023-05-12 16:15:00 +0400

    Added storing

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
     // default state goes here
     // this runs ONE TIME ONLY (unless the user reinstalls your extension)
+    chrome.storage.local.set({
+        name: "Jack"
+    });
 });
