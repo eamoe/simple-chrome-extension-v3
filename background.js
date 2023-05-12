fcf41fa8b5be37522b2c1e0b15bdb3444d8433cc
Eugene Menski <eugene.menski@gmail.com>
2023-05-12 17:04:08 +0400

    Injected content through executeScript & created foreground.js

--- background.js
+++ background.js
@@ -9,5 +9,15 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // Check if the web page is fully loaded and it is the actual web page (http)
     if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+        // Inject content script using the "scripting" API
+        // To insert CSS: chrome.scripting.insertCSS({...
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            files: ["./foreground.js"]
+        })
+        .then(() => {
+            console.log("INJECTED THE FOREGROUND SCRIPT.");
+        })
+        .catch(err => console.log(err));
     }
 });
