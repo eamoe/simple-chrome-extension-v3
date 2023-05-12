78e4607fa820544abace0e41d6b0bdda5c93c835
Eugene Menski <eugene.menski@gmail.com>
2023-05-12 17:38:32 +0400

    Added injecting CSS & onMessage listener

--- background.js
+++ background.js
@@ -9,15 +9,60 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // Check if the web page is fully loaded and it is the actual web page (http)
     if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+        
         // Inject content script using the "scripting" API
-        // To insert CSS: chrome.scripting.insertCSS({...
-        chrome.scripting.executeScript({
+        chrome.scripting.insertCSS({
             target: { tabId: tabId },
-            files: ["./foreground.js"]
+            files: ["./foreground_styles.css"]
         })
+        
         .then(() => {
-            console.log("INJECTED THE FOREGROUND SCRIPT.");
+            console.log("INJECTED THE FOREGROUND STYLES.");
+
+            chrome.scripting.executeScript({
+                target: { tabId: tabId },
+                files: ["./foreground.js"]
+        })
+        
+        .then(() => {
+                        console.log("INJECTED THE FOREGROUND SCRIPT.");
+                    });
         })
         .catch(err => console.log(err));
+    
+    }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === 'get_name') {
+        chrome.storage.local.get('name', data => {
+            if (chrome.runtime.lastError) {
+                sendResponse({
+                    message: 'fail'
+                });
+
+                return;
+            }
+
+            sendResponse({
+                message: 'success',
+                payload: data.name
+            });
+        });
+
+        return true;
+    } else if (request.message === 'change_name') {
+        chrome.storage.local.set({
+            name: request.payload
+        }, () => {
+            if (chrome.runtime.lastError) {
+                sendResponse({ message: 'fail' });
+                return;
+            }
+
+            sendResponse({ message: 'success' });
+        })
+
+        return true;
     }
 });
