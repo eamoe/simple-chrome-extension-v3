5f6a06baf31837f37aa44ceb6d070353e3eb0c3a
Eugene Menski <eugene.menski@gmail.com>
2023-05-12 17:37:56 +0400

    Added form

--- foreground.js
+++ foreground.js
@@ -1,2 +1,45 @@
-document.querySelector("body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img").style.transitionDuration = '3.0s';
-document.querySelector("body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img").style.transform = 'rotate(3000deg)';
+
+// Rotate Google's logo
+//document.querySelector("body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img").style.transitionDuration = '3.0s';
+//document.querySelector("body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img").style.transform = 'rotate(3000deg)';
+
+// Put a "form" on Google's homepage and
+// allows the user to enter a name that gets saved in the Chrome extension
+const ce_main_container = document.createElement('DIV');
+const ce_name = document.createElement('DIV');
+const ce_input = document.createElement('INPUT');
+const ce_button = document.createElement('DIV');
+
+ce_main_container.classList.add('ce_main');
+ce_name.id = 'ce_name';
+ce_input.id = 'ce_input';
+ce_button.id = 'ce_button';
+
+ce_name.innerHTML = `Hello NAME`;
+ce_button.innerHTML = `Change name.`;
+
+ce_main_container.appendChild(ce_name);
+ce_main_container.appendChild(ce_input);
+ce_main_container.appendChild(ce_button);
+
+
+document.querySelector('body').appendChild(ce_main_container);
+
+chrome.runtime.sendMessage({ 
+    message: "get_name"
+}, response => {
+    if (response.message === 'success') {
+        ce_name.innerHTML = `Hello ${response.payload}`;
+    }
+});
+
+ce_button.addEventListener('click', () => {
+    chrome.runtime.sendMessage({ 
+        message: "change_name",
+        payload: ce_input.value
+    }, response => {
+        if (response.message === 'success') {
+            ce_name.innerHTML = `Hello ${ce_input.value}`;
+        }
+    });
+});
